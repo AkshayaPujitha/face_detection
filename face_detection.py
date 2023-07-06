2b869999360d40fb2831dc500764e8871bf0113e
pujitha <akshayapujithakolli@gmail.com>
2023-07-06 22:41:58 +0530

    basic face detection done

--- face_detection.py
+++ face_detection.py
@@ -28,7 +28,7 @@ while True:
     gray_img=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     
     face_coordinates=trained_faces.detectMultiScale(gray_img)
-    print(face_coordinates)
+    #print(face_coordinates)
     for x,y,w,h in face_coordinates:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),5)
     cv2.imshow('my photo',frame)
